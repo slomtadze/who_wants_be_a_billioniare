d6d511993c47e1f82d335cf8513f07a1f52e2700
Saba Lomtadze <slomtadze90@gmail.com>
2022-11-01 10:42:03 +0000

    Update 7 files

--- src/components/active/Trivia.js
+++ src/components/active/Trivia.js
@@ -1,53 +1,66 @@
-/* import "./trivia.css";
-import data from "./Questions";
 import { useEffect, useState } from "react";
-import useSound from "use-sound";
 
-import correct from "../assets/src_sounds_correct.mp3";
-import wrong from "../assets/src_sounds_wrong.mp3"; */
-import { useState } from "react";
 import data from "../../assets/Data/DUMMY_QUESTIONS";
+import Timer from "./Timer";
 import styles from "./Trivia.module.css";
 
 const Trivia = (props) => {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [selectedClassName, setSelectedClassName] = useState(styles.answer);
-
-  const { id } = props;
-
+  const [pauseTimer, setPauseTimer] = useState(false);
+  const { id, onExpire } = props;
   const currentQuestion = data.find((question) => question.id === id);
 
-  const onAnwerConfirm = (answer) => {
-    console.log(answer.text);
-    setSelectedAnswer(answer.text);
-    setSelectedClassName(styles.selected);
+  const correctAnwerHandler = () => {
+    onExpire();
   };
 
-  console.log("Rerenderd");
+  const inCorrectAnswerHandler = () => {};
+
+  const onAnwerConfirm = (answer) => {
+    setSelectedAnswer(answer.text);
+    setSelectedClassName(styles.selected);
+    const timer = setTimeout(() => {
+      if (answer.correct) {
+        correctAnwerHandler();
+        setSelectedAnswer(null);
+      } else {
+        inCorrectAnswerHandler();
+      }
+    }, 3000);
+  };
+
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 30);
+
+  const answerHandler = (answer) => {};
 
   return (
     <div className={styles.trivia}>
-      <div className={styles.timer}>1</div>
+      {currentQuestion && (
+        <Timer expiryTimestamp={time} onTimerExpire={props.onExpire} />
+      )}
       <div className={styles.question}>
         {currentQuestion ? currentQuestion.question : ""}
       </div>
       <div className={styles.answers}>
-        {currentQuestion.answers.map((answer) => {
-          if (selectedAnswer === answer.text) {
-            return <div className={selectedClassName}>{answer.text}</div>;
-          } else if (selectedAnswer !== answer.text && selectedAnswer) {
-            return <div className={styles.blocked}>{answer.text}</div>;
-          } else {
-            return (
-              <div
-                className={styles.answer}
-                onClick={() => onAnwerConfirm(answer)}
-              >
-                {answer.text}
-              </div>
-            );
-          }
-        })}
+        {currentQuestion &&
+          currentQuestion.answers.map((answer) => {
+            if (selectedAnswer === answer.text) {
+              return <div className={selectedClassName}>{answer.text}</div>;
+            } else if (selectedAnswer !== answer.text && selectedAnswer) {
+              return <div className={styles.blocked}>{answer.text}</div>;
+            } else {
+              return (
+                <div
+                  className={styles.answer}
+                  onClick={() => onAnwerConfirm(answer)}
+                >
+                  {answer.text}
+                </div>
+              );
+            }
+          })}
       </div>
     </div>
   );
