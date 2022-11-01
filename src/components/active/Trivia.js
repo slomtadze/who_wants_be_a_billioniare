1881048662eaa938275fc7dee5a3e18720cbe063
Saba Lomtadze <slomtadze90@gmail.com>
2022-11-01 08:15:49 +0000

    Update 3 files

--- src/components/active/Trivia.js
+++ src/components/active/Trivia.js
@@ -11,60 +11,19 @@ import styles from "./Trivia.module.css";
 
 const Trivia = (props) => {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const [className, setClassName] = useState(styles.answer);
+  const [selectedClassName, setSelectedClassName] = useState(styles.answer);
 
   const { id } = props;
 
   const currentQuestion = data.find((question) => question.id === id);
 
   const onAnwerConfirm = (answer) => {
-    setSelectedAnswer(answer);
-    setClassName(styles.correct);
+    console.log(answer.text);
+    setSelectedAnswer(answer.text);
+    setSelectedClassName(styles.selected);
   };
 
-  /*     setSelectedAnswer(a);
-    setClassName("answer active");
-    delay(3000, () => {
-      setClassName(a.correct ? "answer correct" : "answer wrong");
-    }); */
-
-  /*   const { setStop, questionNumber, setQuestionNumber } = props;
-
-  const [question, setQuestion] = useState(null);
-  ;
-  
-
-  const [corectAnswer] = useSound(correct);
-  const [wrongAnswer] = useSound(wrong);
-
-  useEffect(() => {
-    setQuestion(data[questionNumber - 1]);
-  }, [questionNumber]);
-
-  const delay = (duration, callback) => {
-    setTimeout(() => {
-      callback();
-    }, duration);
-  };
-
-
-
-    delay(5000, () => {
-      if (a.correct) {
-        corectAnswer();
-        delay(1000, () => {
-          setQuestionNumber((prev) => prev + 1);
-          selectedAnswer(null);
-        });
-      } else {
-        wrongAnswer();
-        //ჩასამატებელია სწორი პასუხი რომ გამომიჩნდეს ეკრანზე
-        delay(1000, () => {
-          setStop(true);
-        });
-      }
-    });
-  }; */
+  console.log("Rerenderd");
 
   return (
     <div className={styles.trivia}>
@@ -74,15 +33,10 @@ const Trivia = (props) => {
       </div>
       <div className={styles.answers}>
         {currentQuestion.answers.map((answer) => {
-          if (selectedAnswer == answer.text) {
-            return (
-              <div
-                className={styles.answer}
-                onClick={() => onAnwerConfirm(answer)}
-              >
-                {answer.text}
-              </div>
-            );
+          if (selectedAnswer === answer.text) {
+            return <div className={selectedClassName}>{answer.text}</div>;
+          } else if (selectedAnswer !== answer.text && selectedAnswer) {
+            return <div className={styles.blocked}>{answer.text}</div>;
           } else {
             return (
               <div
