f49e6bd84a8df6aaa320faab56823395365191b8
Saba Lomtadze <slomtadze90@gmail.com>
2022-11-09 08:44:44 +0000

    Update 3 files

--- src/components/active/Helps/Helps.js
+++ src/components/active/Helps/Helps.js
@@ -1,16 +1,25 @@
-import styles from "./Helps.module.css";
+import { useCallback, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Help from "./Help";
 import manageQuestionData from "../../../Helpers/manageQuestionData";
 import { setAnswersReducer } from "../../../Store/Question-slice";
 import { IoIosPeople } from "react-icons/io";
 import { FiPhoneCall } from "react-icons/fi";
 
+import styles from "./Helps.module.css";
+
 const Helps = (props) => {
   const { helpIsUsed, setHelpIsUsed } = props;
   const { answers } = useSelector((state) => state.question);
   const dispatch = useDispatch();
+  const peopleIcon = useMemo(() => {
+    return <IoIosPeople />;
+  }, []);
+  const phoneIcon = useMemo(() => {
+    return <FiPhoneCall />;
+  }, []);
 
-  const fiftyClickHanler = () => {
+  const fiftyClickHanler = useCallback(() => {
     if (helpIsUsed.fifty) {
       return;
     } else {
@@ -20,8 +29,8 @@ const Helps = (props) => {
       const editedAnswers = manageQuestionData(answers);
       dispatch(setAnswersReducer(editedAnswers));
     }
-  };
-  const audienceClickHanler = () => {
+  }, []);
+  const audienceClickHanler = useCallback(() => {
     if (helpIsUsed.audience.isUsed) {
       return;
     } else {
@@ -29,8 +38,8 @@ const Helps = (props) => {
         return { ...prev, audience: { isUsed: true, isShown: true } };
       });
     }
-  };
-  const phoneCallClickHanler = () => {
+  }, []);
+  const phoneCallClickHanler = useCallback(() => {
     if (helpIsUsed.phoneCall.isUsed) {
       return;
     } else {
@@ -38,28 +47,25 @@ const Helps = (props) => {
         return { ...prev, phoneCall: { isUsed: true, isShown: true } };
       });
     }
-  };
+  }, []);
 
   return (
     <div className={styles.box}>
-      <div
-        className={helpIsUsed.fifty ? styles.used : styles.help}
+      <Help
+        content="50 : 50"
+        style={helpIsUsed.fifty ? styles.used : styles.help}
         onClick={fiftyClickHanler}
-      >
-        50 : 50
-      </div>
-      <div
-        className={helpIsUsed.audience.isUsed ? styles.used : styles.help}
+      />
+      <Help
+        content={peopleIcon}
+        style={helpIsUsed.audience.isUsed ? styles.used : styles.help}
         onClick={audienceClickHanler}
-      >
-        <IoIosPeople />
-      </div>
-      <div
-        className={helpIsUsed.phoneCall.isUsed ? styles.used : styles.help}
+      />
+      <Help
+        content={phoneIcon}
+        style={helpIsUsed.phoneCall.isUsed ? styles.used : styles.help}
         onClick={phoneCallClickHanler}
-      >
-        <FiPhoneCall />
-      </div>
+      />
     </div>
   );
 };
